cb1b830f827e9ad706427afcb6c352f0d71c12a9
JF-Lalonde <lalondej88@gmail.com>
2017-08-24 12:15:25 -0600

    Passing model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a user from omniauth information" do
+    OmniAuth.config.mock_auth[:reddit] = OmniAuth::AuthHash.new({
+      provider: "reddit",
+      uid: "abc123",
+      info: {
+        name: "Spartacus"
+      },
+      credentials: {
+        token: "tokenstringything"
+      }
+    })
+    User.from_omniauth(OmniAuth.config.mock_auth[:reddit])
+    new_user = User.first
+
+    expect(new_user.uid).to eq("abc123")
+    expect(new_user.username).to eq("Spartacus")
+    expect(new_user.token).to eq("tokenstringything")
+  end
 end
